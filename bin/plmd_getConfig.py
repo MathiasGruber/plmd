c7c568cc86dc3feaad3b84325caaebc3fd759faf
Mathias Gruber <nano.mathias@gmail.com>
2014-10-04 17:47:24 +0200

    Default configuration moved to src folder

---
+++ bin/plmd_getConfig.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+# Start execution
+try:
+    
+    # Import needed modules
+    import argparse, traceback, sys, ConfigParser
+    import plmd.defaultConfig
+    
+    # Argument parsing & help page
+    parser = argparse.ArgumentParser(description=
+    """PLMD (Peptide Ligand Molecular Dynamics) 
+    Retrieves a default configuration file for use with PLMD
+    """)
+    
+    # Get a default config file
+    config = plmd.defaultConfig.getDefaultConfig()
+
+    # Writing our configuration file to 'example.cfg'
+    with open('defaultExample.cfg', 'wb') as configfile:
+        config.write(configfile)
+    
+except Exception as e:
+    
+    # Get into
+    exc_type, exc_value, exc_traceback = sys.exc_info()
+    
+    # Show traceback
+    print "\n\n====================="
+    print "== ERROR TRACEBACK =="
+    print "====================="
+    for err in traceback.format_exception(exc_type, exc_value,exc_traceback):
+        print err
+    print "Script execution was stopped."
